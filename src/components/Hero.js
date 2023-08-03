c2604cbee40d2b28a5dce5f34bb2ce825f02df5c
HalilAladag <hllaladag@gmail.com>
2023-08-03 16:28:40 +0300

    Taiwind-Media-Config-Upt

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ const Hero = ({ language, darkMode }) => {
     tr: {
       greetings:
         'Merhaba, ben Halil! Ben bir full-stack yazılım geliştiricisiyim. Mükemmel kullanıcı deneyimleriyle sağlam ve ölçeklenebilir frontend ürünleri oluşturacak bir geliştirici arıyorsanız. Benimle el sıkışın.',
-      head1: 'Yaratıcı Düşünür  Minimalist Sever',
+      head1: 'Yaratıcı Düşünür Minimalist Sever',
       contactButton: 'İletişim',
       github: 'Github',
       linkedin: 'Linkedin',
@@ -31,55 +31,55 @@ const Hero = ({ language, darkMode }) => {
 
   return (
     <div
-      className={`hero-container flex flex-col items-center ${
+      className={`hero-container flex ${
         darkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'
-      } py-12 md:py-20 rounded-lg shadow-lg px-6 md:px-16 space-y-8`}
+      } py-8 md:py-20 rounded-lg shadow-lg px-6 md:px-16 space-y-8`}
       style={{
         backgroundImage: `url('./Images/sky.png')`,
         backgroundSize: 'cover',
         backgroundPosition: 'left',
       }}
     >
-      <div className="md:w-1/2 md:text-center">
+      <div className="md:flex md:items-center md:w-4/5 mx-auto">
         <animated.img
           src="./Images/HeroPic1.jpg"
           alt="Hero"
-          className="h-48 md:h-80 w-48 md:w-80 rounded-full mb-8 border-4 border-indigo-600 shadow-lg inline-block"
+          className="h-48 md:h-80 w-48 md:w-80 rounded-full mb-8 border-4 border-indigo-600 shadow-lg inline-block md:mr-12"
           style={imageAnimation}
         />
-      </div>
-      <div className="md:w-1/2">
-        <animated.h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8">
-          {head1}
-        </animated.h1>
-        <animated.p className="text-sm md:text-xl mb-4 md:mb-8">
-          {greetings}
-        </animated.p>
-        <div className="flex space-x-4">
-          <button
-            onClick={() => window.open('', '_blank')}
-            className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
-              darkMode ? 'bg-indigo-400 text-gray-900' : 'bg-indigo-600 text-white'
-            } hover:bg-indigo-500 cursor-pointer`}
-          >
-            {contactButton}
-          </button>
-          <button
-            onClick={() => window.open('https://github.com/HalilAladag', '_blank')}
-            className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
-              darkMode ? 'bg-gray-600 text-gray-100' : 'bg-indigo-600 text-white'
-            } hover:bg-gray-700 cursor-pointer`}
-          >
-            {github}
-          </button>
-          <button
-            onClick={() => window.open('https://www.linkedin.com/in/halil-ibrahim-alada%C4%9F-377554168/', '_blank')}
-            className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
-              darkMode ? 'bg-gray-300 text-indigo-600' : 'bg-indigo-600 text-white'
-            } hover:bg-gray-400 cursor-pointer`}
-          >
-            {linkedin}
-          </button>
+        <div>
+          <animated.h1 className="text-6xl md:text-6xl font-bold mb-4 md:mb-8 text-stone-300">
+            {head1}
+          </animated.h1>
+          <animated.p className="text-sm md:text-2xl mb-8 md:mb-8">
+            {greetings}
+          </animated.p>
+          <div className="flex space-x-4">
+            <button
+              onClick={() => window.open('', '_blank')}
+              className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
+                darkMode ? 'bg-indigo-400 text-gray-900' : 'bg-indigo-600 text-white'
+              } hover:bg-indigo-500 cursor-pointer`}
+            >
+              {contactButton}
+            </button>
+            <button
+              onClick={() => window.open('https://github.com/HalilAladag', '_blank')}
+              className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
+                darkMode ? 'bg-gray-600 text-gray-100' : 'bg-indigo-600 text-white'
+              } hover:bg-gray-700 cursor-pointer`}
+            >
+              {github}
+            </button>
+            <button
+              onClick={() => window.open('https://www.linkedin.com/in/halil-ibrahim-alada%C4%9F-377554168/', '_blank')}
+              className={`px-4 py-2 rounded-full font-bold text-sm md:text-lg ${
+                darkMode ? 'bg-gray-300 text-indigo-600' : 'bg-indigo-600 text-white'
+              } hover:bg-gray-400 cursor-pointer`}
+            >
+              {linkedin}
+            </button>
+          </div>
         </div>
       </div>
     </div>
